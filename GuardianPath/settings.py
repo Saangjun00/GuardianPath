9498f08f6f39ef5f49ed001799e5ee56b92a20d2
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-08-02 16:17:18 +0900

    update home.html & createapp users

--- GuardianPath/settings.py
+++ GuardianPath/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # app
-    'home'
+    'home',
+    'users',
 ]
 
 MIDDLEWARE = [
--- GuardianPath/urls.py
+++ GuardianPath/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('/', include('home.urls')),
+    path('users/', include('users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
