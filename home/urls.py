0a593f90ea682453acbb8202c9c160f21fa9bfc4
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-08-19 21:37:21 +0900

    Add feature Change Password

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
