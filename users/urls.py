84baff3be360a9532bbd2b6992e80a0b8ac4c8c1
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-08-19 21:42:46 +0900

    error 해결

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('/register/', views.register_view, name="register_view"),
-    path('/login/', views.login_view, name='login_view'),
-    path('/profile/', views.profile_view, name='profile'),
-    path('/logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name="register_view"),
+    path('login/', views.login_view, name='login_view'),
+    path('profile/', views.profile_view, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
     path('', views.home, name="home"),
 
     # Reset Password
