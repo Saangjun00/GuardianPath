84973ac446df0c34eede35a84a76722d8bf26088
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-08-21 23:09:50 +0900

    통합본

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 def home(request):
-    context = {
-        'KAKAO_CLIENT_ID': settings.SOCIALACCOUNT_PROVIDERS['kakao']['APP']['client_id'],
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('home')
+    template_name = 'registration/password_change.html'  # 경로를 실제 템플릿 파일 경로로 수정
+
+class CustomPasswordChangeDoneView(PasswordChangeDoneView):
+    template_name = 'home.html'
+
+    def dispatch(self, *args, **kwargs):
+        # 메시지를 추가
+        messages.success(self.request, "비밀번호가 성공적으로 변경되었습니다.")
+        return super().dispatch(*args, **kwargs)
