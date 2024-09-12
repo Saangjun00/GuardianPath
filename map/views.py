29a43a49a2b58950dadaa5b3befa4014430c5dd6
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-09-13 00:56:45 +0900

    출발지 도착지 마커 띄우기

--- map/views.py
+++ map/views.py
@@ -4,21 +4,13 @@ from .models import UserRoute, SearchHistory
 from users.models import User
 from django.urls import reverse
 from django.contrib import messages
-import requests
-from django.http import JsonResponse
+from urllib.parse import urlencode
 
 def save_route(request):
     if request.method == 'POST':
         user_type = request.POST.get('user_type')
         departure = request.POST.get('departure')
         destination = request.POST.get('destination')
-        departure_lat = request.POST.get('departure_lat')
-        departure_lon = request.POST.get('departure_lon')
-        destination_lat = request.POST.get('destination_lat')
-        destination_lon = request.POST.get('destination_lon')
-
-        # 세션 데이터 확인 로그 추가
-        print('세션 데이터:', request.session.items())
 
         # 유저 타입이 입력되지 않은 경우
         if not user_type:
@@ -50,20 +42,14 @@ def save_route(request):
                     destination=destination
                 )
 
-            # 세션에 경로 관련 데이터를 저장
-            request.session['user_type'] = user_type
-            request.session['departure'] = departure
-            request.session['destination'] = destination
-            request.session['departure_lat'] = departure_lat
-            request.session['departure_lon'] = departure_lon
-            request.session['destination_lat'] = destination_lat
-            request.session['destination_lon'] = destination_lon
-
-            # 로그로 세션에 저장된 값 확인
-            print(f'Session: {request.session.get("departure_lat")}, {request.session.get("departure_lon")}')
-            print(f'Session: {request.session.get("destination_lat")}, {request.session.get("destination_lon")}')
-
-            return redirect('search_results')
+            # 쿼리 파라미터를 생성하여 리디렉션
+            query_params = {
+                'user_type': user_type,
+                'departure': departure,
+                'destination': destination,
+            }
+            url = reverse('search_results') + '?' + urlencode(query_params)
+            return redirect(url)
 
     return redirect('home')
 
@@ -72,13 +58,9 @@ def search_results(request):
     tmap_api_key = settings.TMAP_API_KEY
 
     # GET 요청으로 전달된 출발지, 도착지 및 유저 타입 정보 가져오기
-    departure = request.session.get('departure')
-    destination = request.session.get('destination')
-    user_type = request.session.get('user_type')
-    departure_lat = request.session.get('departure_lat')
-    departure_lon = request.session.get('departure_lon')
-    destination_lat = request.session.get('destination_lat')
-    destination_lon = request.session.get('destination_lon')
+    departure = request.GET.get('departure')
+    destination = request.GET.get('destination')
+    user_type = request.GET.get('user_type')
 
     # 길찾기 검색 결과 처리 (현재 미구현)
     results = 1
@@ -88,10 +70,6 @@ def search_results(request):
         'departure': departure,
         'destination': destination,
         'user_type': user_type,
-        'departure_lat': departure_lat,
-        'departure_lon': departure_lon,
-        'destination_lat': destination_lat,
-        'destination_lon': destination_lon,
         'results': results,
     }
 
